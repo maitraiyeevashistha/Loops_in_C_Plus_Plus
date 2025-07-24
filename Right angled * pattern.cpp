adfcf0b5336f0fcc8fc6ca73a72ac8ff22edbe6e
maitraiyeevashistha <maitraiyeevashistha@gmail.com>
2025-07-24 11:36:13 +0530

    Update Right angled * pattern.cpp

--- Right angled * pattern.cpp
+++ Right angled * pattern.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    int i,j;
+    int i,j,k;
     int n=5;
-    for(i=1;i<=n;i++){
-        for(j=1;j<=i;j++){
-            cout<<"* ";
-            
+    for( i=1;i<=n;i++){
+        for( j=1;j<=n-i;j++){
+            cout<<" ";
         }
+        for(k=1;k<=i;k++){
+                cout<<"*";
+            }
         cout<<endl;
     }
 
     return 0;
 }
-
-/*output
-* 
-* * 
-* * * 
-* * * * 
-* * * * * 
-*/
+/*output:
+    *
+   **
+  ***
+ ****
+******/
