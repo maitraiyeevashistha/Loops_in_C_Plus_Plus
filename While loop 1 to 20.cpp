ac0c4b7749bfbf1fc4151225ee5c8af5673fef6a
maitraiyeevashistha <maitraiyeevashistha@gmail.com>
2025-07-29 15:10:36 +0530

    Create While loop 1 to 20.cpp

---
+++ While loop 1 to 20.cpp
@@ -0,0 +1,16 @@
+/*Name : Maitraiyee Vashistha 
+PRN : 24070123057
+E&TC A-3*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i=0;
+    while (i< 20){
+        cout<< i+1 << " ";
+        i++;
+        cout<<endl;
+        }
+    
+    return 0;
+}
